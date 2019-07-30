b7e729d4cd593fae95ca5847cd153fde614fd684
Dijkstra <contiemathias@gmail.com>
2019-07-30 11:34:54 +0200

    update1

---
+++ src/app/graphics/tennis-player/tennis-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tennis-player',
+  templateUrl: './tennis-player.component.html',
+  styleUrls: ['./tennis-player.component.css']
+})
+export class TennisPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
